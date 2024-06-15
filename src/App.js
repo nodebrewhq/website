f004cf7cfb09cb209319542c33b73d4df60195ac
Tasmiya-k <tasmiyakhan2798@gmail.com>
2024-06-15 23:23:40 +0530

    added-responsiveness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-  ChakraProvider,
-  Box
-} 
-from "@chakra-ui/react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import About from './components/About';
@@ -13,26 +9,25 @@ import Upcoming from './components/Upcoming';
 import GetInvolvedSection from './components/GetInvolved';
 import Footer from './components/Footer';
 import SubscriptionSection from './components/Email';
+import theme from './theme'; // Ensure you have the theme file set up
 
 function App() {
-
-return (
-<ChakraProvider>
-      <Box minHeight="100vh" bg="white">
+  return (
+    <ChakraProvider theme={theme}>
+      <Box minHeight="100vh" bg="white" overflowX="hidden">
         <Navbar />
         <LandingPage />
-        < About />
-        < Program />
-        < Upcoming />
-        < GetInvolvedSection />
-        < Sponsor />
-        < Team />
-        < SubscriptionSection />
-        < Footer />
-        {/* Your other components and routes go here */}
+        <About />
+        <Program />
+        <Upcoming />
+        <GetInvolvedSection />
+        <Sponsor />
+        <Team />
+        <SubscriptionSection />
+        <Footer />
       </Box>
     </ChakraProvider>
   );
 }
 
-export default App
+export default App;
